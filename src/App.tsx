2eef96e34a79a98621ad4ee0bfe380cc48beb9d9
Caio Weber Stuart <caiostuart06@gmail.com>
2022-08-17 15:50:25 -0300

    estilizando

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
-import { Cabecalho, Corpo, TítuloCabecalho } from './styles';
+import { Cabecalho, Corpo, Principal, TítuloDoCabecalho, Imagem, Título, Filmes, Filme, Botao } from './styles';
 import './App.css';
 
 function App() {
+
+  const [filmes, setFilmes] = useState([
+    {
+      id: 1,
+      title: 'The Lord of the Rings - The Fellowship Ring',
+      image: 'https://upload.wikimedia.org/wikipedia/pt/3/38/Lord_of_the_Rings_Fellowship_of_the_Ring.jpg',
+      name: 'The Lord of the Rings'
+    },
+    {
+      id: 2,
+      title: 'The Lord of the Rings - The Two Towers',
+      image: 'https://upload.wikimedia.org/wikipedia/pt/5/59/The_Lord_of_the_Rings_The_Two_Towers.jpg',
+      name: 'The Lord of the Rings'
+    },
+    {
+      id: 3,
+      title: 'The Lord of the Rings - The Return of the King',
+      image: 'https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg',
+      name: 'The Lord of the Rings'
+    }
+  ])
+
+  fetch('https://api.themoviedb.org/3/movie/550?api_key=ce55d6ef64d74744c9ce770e6229d6e0', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  .then((resp) => resp.json)
+  .then((data: any) => 
+    console.log(data)
+  )
+  .catch((err) => console.log(err));
+
   return(
     <Corpo>
       <Cabecalho>
         <img src={require('./img/logo.png')} className="cabecalho__imagem1" alt=""/>
-        <TítuloCabecalho>BAGGINSFLIX</TítuloCabecalho>
+        <TítuloDoCabecalho>CLASSICFLIX</TítuloDoCabecalho>
         <img src={require('./img/navbar.png')} className="cabecalho__imagem2" alt=""/>
       </Cabecalho>
       <hr/>
+      <Principal>
+        <Filmes>
+          {filmes.map((filme) => (
+            <Filme>
+            <Imagem src={filme.image} alt=""/>
+            <Título>{filme.name}</Título>
+            <Botao>Detalhes</Botao>
+            </Filme>
+          ))}
+        </Filmes>
+      </Principal>
     </Corpo>
   )
 }
--- src/styles.ts
+++ src/styles.ts
@@ -20,7 +20,51 @@ height: 15vh;
 color: white;
 `;
 
-export const TítuloCabecalho = styled.h1`
+export const TítuloDoCabecalho = styled.h1`
 font-size: 1.5rem;
 font-weight: 400;
+`;
+
+export const Principal = styled.main`
+margin-top: 4rem;
+`;
+
+export const Filmes = styled.section`
+display: flex;
+justify-content: space-around;
+`;
+
+export const Filme = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+gap: 1rem;
+
+padding: 1rem;
+
+background-color: rgb(10, 10, 10)
+`;
+
+export const Botao = styled.button`
+padding: 0.5rem;
+
+background-color: white;
+color: black;
+font-size: 1.2rem;
+font-weight: 400;
+
+width: 75%;
+`;
+
+export const Imagem = styled.img`
+width: 17.5rem;
+height: 22.5rem;
+`;
+
+export const Título = styled.h2`
+color: white;
+font-size: 1.1rem;
+font-weight: 700;
+font-family: 'Rajdhani', sans-serif;
 `;
